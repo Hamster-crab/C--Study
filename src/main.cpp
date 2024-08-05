fe33637e2cd5ab10d44d91bc8c8af52000a481a6
Hamster-Crab <hhidekitano@gmail.com>
2024-08-05 19:29:33 +0900

    .o

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,16 @@
 #include <iostream>
 
-void calc()
+int main ()
 {
-    int num1, num2, Ans;
-    std::string word;
-
-    std::cin >> num1;
-    std::cin >> word;
-    std::cin >> num2;
-
-    if (word == "+")
-    {
-        Ans = num1 + num2;
-    }
-    else if (word == "-")
-    {
-        Ans = num1 - num2;
-    }
-    else if (word == "*")
-    {
-        Ans = num1 * num2;
-    }
-    else if (word == "/")
-    {
-        Ans = num1 / num2;
-    }
-    else if (word == "%")
-    {
-        Ans = num1 % num2;
-    }
-}
-
-int main() {
     while (true) {
-        calc();
-        std::string ic;
-        std::cin >> ic;
-        if (ic == "=") {
-            std::cout << Ans << std::endl;break;
-        } else {
-            calc();
+        std::string Test;
+        std::cin >> Test;
+        if (Test.empty())
+        {}
+        else {
+            std::cout << Test << std::endl;
+            break;
         }
-        return 0;
     }
+    return 0;
 }
